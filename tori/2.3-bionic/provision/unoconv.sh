e1c0d81d3eb06a55974e5e8f1f3689cde4f3aee9
RubyClickAP <ruby@click-ap.com>
2021-01-14 11:14:40 +0800

    Add gem install kramdown

--- tori/2.3-bionic/provision/unoconv.sh
+++ tori/2.3-bionic/provision/unoconv.sh
@@ -10,11 +10,12 @@ apt-get install ttf-wqy-microhei
 #chown www-data:www-data /var/www/.config
 
 # ubuntu upstart
-#cp /vagrant/unoconvd /etc/init.d/unoconvd
-wget -qO /etc/init.d/unoconvd https://gist.githubusercontent.com/RubyClickAP/47948257bfb419d86a79ddd2088cd4fa/raw/b6206ea178c875b75beb22a5984b6e61ab90876f/unoconv-bionic-upstart
+#wget -qO /etc/init.d/unoconvd https://gist.githubusercontent.com/RubyClickAP/47948257bfb419d86a79ddd2088cd4fa/raw/b6206ea178c875b75beb22a5984b6e61ab90876f/unoconv-bionic-upstart
+cp /home/vagrant/unoconvd /etc/init.d/unoconvd
 chmod +x /etc/init.d/unoconvd
 update-rc.d -f unoconvd defaults
 systemctl daemon-reload
 #service unoconvd enable
 service unoconvd start
 service unoconvd status
+service unoconvd stop
